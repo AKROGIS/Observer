38cf9a570044f98190614c1e71cd3a56c09aadee
Regan Sarwas (work mac) <regan_sarwas@nps.gov>
2013-07-12 09:48:30 -0800

    Create a new AGSMapView when the basemap changes

--- Observer/ObserverMapViewController.m
+++ Observer/ObserverMapViewController.m
@@ -13,7 +13,7 @@
 @interface ObserverMapViewController ()
 
 @property (strong, nonatomic) BaseMapManager *maps;
-@property (weak, nonatomic) IBOutlet AGSMapView *mapView;
+@property (strong, nonatomic) AGSMapView *mapView;
 @property (weak, nonatomic) IBOutlet UILabel *mapLabel;
 
 @end
@@ -37,24 +37,57 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+    [self setOrResetBasemap:self.maps.currentMap];
+}
+
+//lazy instantiation
+- (AGSMapView *)mapView
+{
+    if (!_mapView)
+    {
+        _mapView = [[AGSMapView alloc] initWithFrame:self.view.bounds];
+        _mapView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
+        [self.view insertSubview:_mapView aboveSubview:self.mapLabel];
+    }
+    return _mapView;
 }
 
 - (void) viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
 
-    //see http://robsprogramknowledge.blogspot.com/2012/05/back-segues.html
+    //see http://robsprogramknowledge.blogspot.com/2012/05/back-segues.html for handling 'reverse seques'
+    
+    [self setOrResetBasemap:self.maps.currentMap];
+}
 
-    BaseMap *currentMap = [self.maps currentMap];
-    if (currentMap.tileCache)
+- (void) setOrResetBasemap:(BaseMap *)baseMap
+{
+    //A "loaded" mapview has an immutable SR and maxExtents based on the first layer loaded (not on the first layer in the mapView)
+    //removing all layers from the mapview does not "unload" the mapview.
+    //to reset the mapView extents/SR, we need to create a new mapview.
+    //first we need to get the layers, so we can re-add them after setting the new basemap.
+    if (!baseMap.tileCache)
     {
-        self.mapLabel.text = currentMap.name;
-        if ([self.mapView.mapLayers count] > 0)
-            [self.mapView removeMapLayer:self.mapView.mapLayers[0]];
-        [self.mapView insertMapLayer:currentMap.tileCache atIndex:0];
-    }
-    else {
         self.mapLabel.text = @"No background map selected. Click the Maps button to select one.";
+    }
+    else
+    {
+        if (!self.mapView.loaded)
+        {
+            [self.mapView addMapLayer:baseMap.tileCache];
+        }
+        else
+        {
+            if ([self.mapView.mapLayers count] == 0 || baseMap.tileCache != self.mapView.mapLayers[0])
+            {
+                NSMutableArray *layers = [NSMutableArray arrayWithArray: self.mapView.mapLayers];
+                layers[0] = baseMap.tileCache;
+                [self.mapView removeFromSuperview];
+                self.mapView = nil;
+                for (AGSLayer *layer in layers)
+                    [self.mapView addMapLayer:layer];
+            }
+        }
     }
 }
 
