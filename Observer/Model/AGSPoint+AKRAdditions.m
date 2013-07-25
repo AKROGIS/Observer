5a1cfbd5c6ebe0c74e1bf6ada8044e208e845bc0
Regan Sarwas (work mac) <regan_sarwas@nps.gov>
2013-07-24 16:35:34 -0800

    fixed AGSPoint Aditions

--- Observer/Model/AGSPoint+AKRAdditions.m
+++ Observer/Model/AGSPoint+AKRAdditions.m
@@ -16,8 +16,8 @@
     //angle is clockwise from North, convert to math angle: counterclockwise from East = 0
     angle = 90.0 - angle;
     angle = angle * M_PI / 180.0; //make it radians
-    double deltaX = distance * cos(angle);
-    double deltaY = distance * sin(angle);
+    double deltaX = isnan(distance) ? 0 : distance * cos(angle);
+    double deltaY = isnan(distance) ? 0 : distance * sin(angle);
     return [AGSPoint pointWithX:self.x + deltaX y:self.y + deltaY spatialReference:self.spatialReference];
 }
 
