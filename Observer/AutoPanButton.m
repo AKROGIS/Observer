faa5db01599c75964703d86678a7c89b067659fb
Regan Sarwas (work mac) <regan_sarwas@nps.gov>
2014-01-14 10:46:04 -0900

    New AutoPan/rotate button images

--- Observer/AutoPanButton.m
+++ Observer/AutoPanButton.m
@@ -8,25 +8,61 @@
 
 #import "AutoPanButton.h"
 
+@interface AutoPanButton ()
+
+@property (nonatomic, strong) UIImage *gpsOff;
+@property (nonatomic, strong) UIImage *gpsOn;
+@property (nonatomic, strong) UIImage *gpsRotate;
+
+@end
+
 @implementation AutoPanButton
+
+
+- (UIImage *)gpsOff
+{
+    if (!_gpsOff) {
+        _gpsOff = [UIImage imageNamed:@"GpsOff"];
+    }
+    return _gpsOff;
+}
+
+- (UIImage *)gpsOn
+{
+    if (!_gpsOn) {
+        _gpsOn = [UIImage imageNamed:@"GpsOn"];
+    }
+    return _gpsOn;
+}
+
+- (UIImage *)gpsRotate
+{
+    if (!_gpsRotate) {
+        _gpsRotate = [UIImage imageNamed:@"GpsRotate"];
+    }
+    return _gpsRotate;
+}
 
 - (void)turnOff
 {
     //TODO: replace text with cool images/highlighting
-    self.title = @"Off";
-    self.image = nil;
+    //self.title = @"Off";
+    self.title = nil;
+    self.image = self.gpsOff;
 }
 
 - (void)turnOnWithoutRotate
 {
-    self.title = @"On";
-    self.image = nil;
+    //self.title = @"On";
+    self.title = nil;
+    self.image = self.gpsOn;
 }
 
 - (void)turnOnWithRotate
 {
-    self.title = @"OnR";
-    self.image = nil;
+    //self.title = @"OnR";
+    self.title = nil;
+    self.image = self.gpsRotate;
 }
 
 @end
