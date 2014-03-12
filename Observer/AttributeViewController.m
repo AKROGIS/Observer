b14101612f72b4211fc3c864efcee165f36abf53
Regan Sarwas <rsarwas@gmail.com>
2014-03-11 21:31:38 -0700

    removed unnecessary code, and obsolete todo

--- Observer/AttributeViewController.m
+++ Observer/AttributeViewController.m
@@ -14,27 +14,4 @@
 
 @implementation AttributeViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-//TODO: have the view controller updated the managedobject as attributes are changed.
-
 @end
