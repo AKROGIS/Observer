5a5a73bce218d6c3f8b3db9f0d3820743434089a
Regan Sarwas (work mac) <regan_sarwas@nps.gov>
2014-04-04 16:43:08 -0800

    With 7.1 distribution must be over https

--- Observer/ObserverAppDelegate.m
+++ Observer/ObserverAppDelegate.m
@@ -17,7 +17,7 @@
 
 #define kAlertViewNewProtocol      1
 #define kAlertViewNewVersion       2
-#define kAppDistributionPlist      @"http://akrgis.nps.gov/observer/Park_Observer.plist"
+#define kAppDistributionPlist      @"https://akrgis.nps.gov/observer/Park_Observer.plist"
 
 
 @interface ObserverAppDelegate()
