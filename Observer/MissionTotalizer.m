ec47e0d5ac85a7b635e1abe74880b175254545ca
Regan Sarwas <regan_sarwas@nps.gov>
2016-04-29 10:35:24 -0800

    Fully implement the totalizer

--- Observer/MissionTotalizer.m
+++ Observer/MissionTotalizer.m
@@ -28,6 +28,11 @@ typedef NS_OPTIONS(NSUInteger, TotalizerUnits) {
 @property (nonatomic, readwrite) BOOL showOff;
 @property (nonatomic, readwrite) BOOL showTotal;
 @property (nonatomic, readwrite) TotalizerUnits units;
+@property (nonatomic, readwrite) NSString *unitLabel;
+@property (nonatomic, readwrite) NSString *names;
+@property (nonatomic, readwrite) NSString *joinedValues;
+@property (nonatomic, readwrite) double priorOnTotal;
+@property (nonatomic, readwrite) double priorOffTotal;
 
 @end
 
@@ -48,48 +53,159 @@ typedef NS_OPTIONS(NSUInteger, TotalizerUnits) {
     return self;
 }
 
+
+
 - (void)updateWithLocation:(CLLocation *)location forMissionProperties:(MissionProperty *)missionProperty
 {
-    [self updateWMessage];
+    [self updateMessage];
 }
 
 - (void)trackLogSegmentsChanged:(NSMutableArray *)trackLogSegments
 {
     self.trackLogSegments = trackLogSegments;
     self.currentSegment = (TrackLogSegment *)[self.trackLogSegments lastObject];
-    [self updateWMessage];
+    [self updateFieldValues];
+    [self updatePriorTotals];
+    [self updateMessage];
 }
 
 - (void)missionPropertyChanged:(MissionProperty *)missionProperty
 {
-    [self updateWMessage];
+    [self updateFieldValues];
+    [self updatePriorTotals];
+    [self updateMessage];
 }
 
-- (void)updateWMessage
+
+
+- (void)updateFieldValues
 {
-    //_curentSegment.length
-    self.message = [NSString stringWithFormat:@"Track %u time: %@", [self.trackLogSegments count], self.timeString];
+    NSMutableArray *values = [self.fields mapObjectsUsingBlock:^id(NSAttributeDescription *obj, NSUInteger idx) {
+        return [NSString stringWithFormat:@"%@", [self.currentSegment.missionProperty valueForKey:obj.name]];
+    }];
+    self.joinedValues = [values componentsJoinedByString:@";"];
 }
 
-- (NSString *)timeString
+- (void)updatePriorTotals
 {
-    NSTimeInterval duration = self.currentSegment.duration;
-    if (duration < 60) {
-        return [NSString stringWithFormat:@"%0.0f seconds", duration];
+    self.priorOnTotal = 0;
+    self.priorOffTotal = 0;
+    NSUInteger length = self.trackLogSegments.count;
+    if (length < 1) return;
+    for (TrackLogSegment *segment in [self.trackLogSegments subarrayWithRange:NSMakeRange(0, length-1)]) {
+        if ([self isMatchingSegment:segment]) {
+            if (segment.missionProperty.observing) {
+                if (self.units == TotalizerUnitsTime) {
+                    self.priorOnTotal += segment.duration;
+                } else {
+                    self.priorOnTotal += segment.length;
+                }
+            } else {
+                if (self.units == TotalizerUnitsTime) {
+                    self.priorOffTotal += segment.duration;
+                } else {
+                    self.priorOffTotal += segment.length;
+                }
+            }
+        }
     }
-    return [NSString stringWithFormat:@"%0.0f minutes", (duration/60.0)];
 }
 
-//    ⁃	fields - A required array of field names
-//               when any of the fields change, a different total is displayed.
-//               There must be at least on field (string) in the array which matches the name of one of the attributes in the mission
-//    ⁃	units - An optional element with a value of "kilometers" or "miles" or "minutes". Default is "kilometers"
-//    ⁃	includeon - A boolean value (true/false), that indicate is the total while "observing" is true should be displayed.  The default is  true
-//    ⁃	includeoff - A boolean value (true/false), that indicate if the total while "observing" is false should be displayed.  The default is  false
-//    ⁃	includetotal - A boolean value (true/false), that indicate if the total regardless of "observing" status should be displayed.  The default is  false
+- (BOOL)isMatchingSegment:(TrackLogSegment *)segment
+{
+    for (NSAttributeDescription *attribute in self.fields) {
+        NSString *key = attribute.name;
+        id currentValue = [self.currentSegment.missionProperty valueForKey:key];
+        id segmentValue = [segment.missionProperty valueForKey:key];
+        if (!currentValue && !segmentValue) {
+            // nil and nil is a match, even though it fails equality
+            //Good so far, check next attribute
+            continue;
+        }
+        if ([currentValue isEqual:segmentValue]) {
+            //Good so far, check next attribute
+            continue;
+        }
+        return NO;
+    }
+    return YES;
+}
+
+- (void)updateMessage
+{
+    double onTotal = self.priorOnTotal;
+    double offTotal = self.priorOffTotal;
+    double value = 0;
+    if (self.units == TotalizerUnitsTime) {
+        value = self.currentSegment.duration;
+    } else {
+        value = self.currentSegment.length;
+    }
+    if (self.currentSegment.missionProperty.observing) {
+        onTotal += value;
+    } else {
+        offTotal += value;
+    }
+    //Convert from meters or seconds
+    if (self.units == TotalizerUnitsMiles) {
+        onTotal = onTotal/1609.34;  //1200.0*5280.0/3937.0;
+        offTotal = offTotal/1609.34;
+    }
+    if (self.units == TotalizerUnitsKilometers) {
+        onTotal = onTotal/1000.0;
+        offTotal = offTotal/1000.0;
+    }
+    if (self.units == TotalizerUnitsTime) {
+        onTotal = onTotal/60.0;
+        offTotal = offTotal/60.0;
+    }
+    double totalTotal = onTotal + offTotal;
+    self.message = [self buildMessageOn:onTotal off:offTotal total:totalTotal];
+}
+
+-(NSString *)buildMessageOn:(double)on off:(double)off total:(double)total
+{
+    NSString *message = nil;
+    if (self.showOn && self.showOff && self.showTotal)
+    {
+        message = [NSString stringWithFormat:@"%0.1f on + %0.1f off in %0.1f %@ for %@ %@",on, off, total, self.unitLabel, self.names, self.joinedValues];
+    } else
+        if (self.showOn && self.showOff)
+    {
+        message = [NSString stringWithFormat:@"%0.1f on + %0.1f off %@ for %@ %@",on, off, self.unitLabel, self.names, self.joinedValues];
+    } else
+        if (self.showOn && self.showTotal)
+    {
+        message = [NSString stringWithFormat:@"%0.1f on in %0.1f %@ for %@ %@", on, total, self.unitLabel, self.names, self.joinedValues];
+    } else
+        if (self.showOff && self.showTotal)
+    {
+        message = [NSString stringWithFormat:@"%0.1f off in %0.1f %@ for %@ %@", off, total, self.unitLabel, self.names, self.joinedValues];
+    } else
+        if (self.showTotal)
+    {
+        message = [NSString stringWithFormat:@"%0.1f %@ total for %@ %@", total, self.unitLabel, self.names, self.joinedValues];
+    } else
+        if (self.showOff)
+    {
+        message = [NSString stringWithFormat:@"%0.1f %@ off %@ %@", off, self.unitLabel, self.names, self.joinedValues];
+    } else
+        if (self.showOn)
+    {
+        message = [NSString stringWithFormat:@"%0.1f %@ on %@ %@", on, self.unitLabel, self.names, self.joinedValues];
+    }
+    return message;
+}
 
 - (BOOL)parseProtocol
 {
+    // From the Protocol Specifications:
+    //    ⁃	fields - A required array of field names
+    //    ⁃	units - An optional element with a value of "kilometers" or "miles" or "minutes". Default is "kilometers"
+    //    ⁃	includeon - A boolean value (true/false), that indicate is the total while "observing" is true should be displayed.  The default is  true
+    //    ⁃	includeoff - A boolean value (true/false), that indicate if the total while "observing" is false should be displayed.  The default is  false
+    //    ⁃	includetotal - A boolean value (true/false), that indicate if the total regardless of "observing" status should be displayed.  The default is  false
+
     BOOL hasTotalizer = NO;
     switch (self.protocol.metaversion) {
         case 2:
@@ -110,11 +226,14 @@ typedef NS_OPTIONS(NSUInteger, TotalizerUnits) {
                     self.showTotal = YES;
                 }
                 self.units = TotalizerUnitsKilometers;
+                self.unitLabel = @"Kms";
                 if ([config[@"units"] isEqual:@"miles"]) {
                     self.units = TotalizerUnitsMiles;
+                    self.unitLabel = @"Miles";
                 }
                 if ([config[@"units"] isEqual:@"minutes"]) {
                     self.units = TotalizerUnitsTime;
+                    self.unitLabel = @"Minutes";
                 }
                 // get and verify fields
                 NSMutableArray *tempfields = [NSMutableArray new];
@@ -124,8 +243,18 @@ typedef NS_OPTIONS(NSUInteger, TotalizerUnits) {
                     for (id protocol_field in fields) {
                         if ([protocol_field isKindOfClass:[NSString class]]) {
                             NSString *field = (NSString *)protocol_field;
+                            NSString *obscuredField = [NSString stringWithFormat:@"%@%@",kAttributePrefix,field];
                             // look up in self.protocol.missionFeature.attributes and add to array if it exists
-                            [tempfields addObject:field];
+                            for (NSAttributeDescription *attribute in self.protocol.missionFeature.attributes) {
+                                if ([obscuredField isEqualToString:attribute.name]) {
+                                    if (!self.names) {
+                                        self.names = field;
+                                    } else {
+                                        self.names = [NSString stringWithFormat:@"%@;%@", self.names, field];
+                                    }
+                                    [tempfields addObject:attribute];
+                                }
+                            }
                         }
                     }
                 }
@@ -137,6 +266,7 @@ typedef NS_OPTIONS(NSUInteger, TotalizerUnits) {
             }
         }
     }
+
     return hasTotalizer;
 }
 
