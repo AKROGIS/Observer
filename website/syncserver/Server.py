447b10a171ea3a0e6bbac8033266a58afc6fa700
Regan Sarwas <regan_sarwas@nps.gov>
2017-05-05 13:54:02 -0800

    Make sync work over https on server
    Cert and private key are not shared in the repo.  They are private and
    specific to the server, so look there.  A fully qualified domain name
    is required, and the cert is signed by DOI, so the client must have the
    DOIRootCA2 installed.  This will be true for all iPads on the NPS
    network.

--- website/syncserver/Server.py
+++ website/syncserver/Server.py
@@ -116,5 +116,5 @@ if not os.path.exists(SyncHandler.upload_folder):
     os.makedirs(SyncHandler.upload_folder)
 # For more info on https see: https://gist.github.com/dergachev/7028596
 server = HTTPServer(('', 8443), SyncHandler)
-server.socket = ssl.wrap_socket (server.socket, certfile='./akrgis.pem', server_side=True)
+server.socket = ssl.wrap_socket (server.socket, keyfile='key.pem', certfile='cert.pem', server_side=True)
 server.serve_forever()
